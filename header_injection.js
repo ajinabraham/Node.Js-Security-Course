23d304ba488dc7c9a55758de48bf70be95ffbddb
Ajin Abraham <ajin25@gmail.com>
2020-05-21 19:32:02 -0400

    v5 check

--- header_injection.js
+++ header_injection.js
@@ -2,5 +2,4 @@ ar express = require('express');
 var app = express();
 app.get('/', function (req, res) {
     res.writeHead(200, { test: 'foo \r\ninvalid: bar' + req.foo });
-    res.set('Content-Type', req.query.foo);
 });
